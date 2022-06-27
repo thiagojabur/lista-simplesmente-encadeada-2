7746d6f57138ceca417dcbe7491214ae80ec0697
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2022-06-27 15:51:39 -0300

    Update principal.java

--- lista-simplesmente-encadeada/src/principal.java
+++ lista-simplesmente-encadeada/src/principal.java
@@ -2,16 +2,25 @@
 public class principal {
 
 	public static void main(String[] args) {
-		No node1, node2;
-		node1 = new No(4);
+		No node1, node2, node3, node4;
+		node1 = new No(9);
 		node2 = new No(3);
+		node3 = new No(2);
+		node4 = new No(3);
 		Lista lista = new Lista();
 		lista.inserir(node1);
 		lista.inserir(node2);
-		lista.inserir(new No(6));
+		lista.inserir(node3);
+		lista.inserir(node4);
 		System.out.println(lista);
-		System.out.println("o primeiro nó é o " + lista.consultarPrimeiro());
+		System.out.println("o primeiro nÃ³ Ã© o " + lista.consultarPrimeiro());
 		System.out.println(lista.buscar(6));
+		System.out.println(lista.temRepetidos());
+		lista.remover(node1);
+		lista.remover(node4);
+		System.out.println(lista);
+		lista.remover(node3);
+		
 	}
 
 }
